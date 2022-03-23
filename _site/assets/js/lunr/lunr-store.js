279b8a02498eb260ecc330668cccb3b0464d4f50
kyj0206 <gansoo.mayo@gmail.com>
2022-03-23 14:36:54 +0900

    2022-03-23

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -35,8 +35,8 @@ var store = [{
         "url": "/iterm2/iTerm2/",
         "teaser": "/assets/images/my_profile.png"
       },{
-        "title": "[CodePen] Jekyll 블로그에 CodePen 추가하는 방법",
-        "excerpt":" ","categories": ["CodePen"],
+        "title": "[CodePen] 블로그에 CodePen Embed하는 방법",
+        "excerpt":" 안녕하세요. Kim.YJ 입니다.   해당 포스팅은 Jekyll 블로그에서 CodePen을 추가하는 방법에 대한 설명입니다.   앞으로의 포스팅에서 이해를 돕기 위해 CodePen을 적용 할 것입니다.   실시간 코드에디터인 CodePen을 알아보고 Html, Css, Js을 손쉽게 CodePen으로 이용하는 방법을 포스팅 하겠습니다.     [CodePen 이란?]          CodePen은 Web_Developer를 위한 Online_Compiler입니다.   쉽게말해 웹 개발자를 기준으로 만들어지다 보니 웹언어에 최적화 되어있다고 볼 수 있으며, 가장 좋은 점은 소스코드를 적용해 결과를 한번에 볼 수 있다는 장점이있습니다.   간단하게 codepen을 직접 블로그에 Embed해 보았습니다.      See the Pen    Untitled by K.Y.J (@kyj0206)   on CodePen.    이렇게 적용할 수 있고, 적용 방법 또한, 어렵지 않으니 천천히 진행해 보겠습니다.   [Codepen Embed 방법]   1. 아래 링크를 통해 Codepen에 접속   [ Link : Codepen ]   2. 계정 생성 및 로그인 진행   우측 상단의 Sign Up 또는 Log In 버튼을 이용하여 진행해 줍니다.         GitHub 계정 으로 쉽게 연동가능 합니다.        3. New Pen을 생성   우측 상단 스마일 아이콘은 누르면 아래 이미지와 같은 창을 볼 수 있습니다.    New Pen 을 클릭해서 새로운 Pen을 만들어 줍니다.        4. 간단한 코드 작성 후 저장   상단의 [ Save ] 버튼 or [ Ctrl+S ] 으로 저장을 해줍니다.        5. CodePen Embed   우측 하단의 Embed 버튼 클릭후 Iframe 탭으로 이동하여 소스 코드 복사해 줍니다.        6. 블로그 포스팅 페이지에 적용   복사한 소스코드를 블로그 포스팅 으로 옮겨줍니다.  Local서버로 확인 결과,아래 이미지와 같이 정상적으로 화면에 출력되어 집니다.        마지막으로 Commit 하여 Push 해주고 확인해 주면 끝 입니다.      지금까지 블로그에서 CodePen을 추가하는 방법을 배워 보았습니다.      CodePen을 접목하여,    앞으로 블로그를 멋지게 관리해 보세요.      다음 포스팅은 더 유익한 정보로 찾아뵙겠습니다.    감사합니다!   ","categories": ["CodePen"],
         "tags": ["Jekyll","Blog","CodePen"],
         "url": "/codepen/CodePen-Blog/",
         "teaser": "/assets/images/my_profile.png"
