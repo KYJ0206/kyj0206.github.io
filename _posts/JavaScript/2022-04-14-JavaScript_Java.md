9aa6715b76c97560b1b0e325f8cc3e14304f8bb9
kyj0206 <gansoo.mayo@gmail.com>
2022-04-14 17:20:16 +0900

    2022-04-14

---
+++ _posts/JavaScript/2022-04-14-JavaScript_Java.md
@@ -0,0 +1,23 @@
+---
+title: "[JavaScript]와 Java의 차이점과 특징"
+excerpt: "JavaScript와 Java의 다른점을 알아보자"
+categories:
+  - JavaScript
+tags:
+  - [JavaScript]
+toc: true
+toc_sticky: true
+published: true
+
+date: 2022-04-14
+last_modified_at: 2022-04-14
+---
+
+---
+
+<span style='font-size:1rem'>**안녕하세요. Kim.YJ 입니다.**</span>
+
+<span style='font-size:1rem'>**이번 포스팅은 JavaScript와 Java의,**</span> <br>
+<span style='font-size:1rem'>**차이점과 특징을 자세히 알아 보겠습니다.**</span>
+
+---
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -70,4 +70,10 @@ var store = [{
         "tags": ["JavaScript"],
         "url": "/javascript/JavaScript/",
         "teaser": "/assets/images/my_profile.png"
+      },{
+        "title": "[JavaScript]와 Java의 차이점과 특징",
+        "excerpt":"  안녕하세요. Kim.YJ 입니다.   이번 포스팅은 JavaScript와 Java의,   차이점과 특징을 자세히 알아 보겠습니다.    ","categories": ["JavaScript"],
+        "tags": ["JavaScript"],
+        "url": "/javascript/JavaScript_Java/",
+        "teaser": "/assets/images/my_profile.png"
       }]
